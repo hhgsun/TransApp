c96248970f2f37dc614d59abb3d0983a71d3fdf9
hhgsun <hhgsun@gmail.com>
2016-12-13 02:17:07 +0300

    anasayfa ilanlar

--- src/pages/anasayfa/anasayfa.ts
+++ src/pages/anasayfa/anasayfa.ts
@@ -1,30 +1,111 @@
-import { Component, ViewChild } from '@angular/core';
-import { NavController, Slides, AlertController } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
 import { AngularFire } from 'angularfire2';
 
 import { SearchComponent } from '../components/search'
 import { IlanverPage } from '../ilanver/ilanver'
+import { IlandetayPage } from '../ilandetay/ilandetay'
 
 @Component({
   selector: 'page-anasayfa',
   templateUrl: 'anasayfa.html'
 })
 export class AnasayfaPage {
+  /* Anasayfa Slider Aktif olduğu zaman bu kısım aktif edilmeli
   @ViewChild('homeSlider') slider: Slides;
   public topTabs = "ilanlar";
-  public test: any;
+  */
+
+  public ilanlar: any = [];
+  public ilanSayisi = 10;
+  public infiniteDahaFazla = true;
+  public gelenIlanlarLength = 0;
+
   constructor(public navCtrl: NavController, public angularFire: AngularFire, public alertCtrl: AlertController) {
-    this.test = {baslik: 'test'};
+
   }
 
   ionViewDidLoad() {
-    // console.log("Anasayfa");
+    this.angularFire.database.list("ilanlar", {
+      query: { limitToLast: this.ilanSayisi }
+    }).subscribe((gelenIlanlar) => {
+      // her loadIlanlardan gelen ilanların length ini tutarız önceki ile eşit ise btnDahaFazla false olur
+      if (this.gelenIlanlarLength == gelenIlanlar.length) {
+        this.infiniteDahaFazla = false;
+      } else {
+        this.gelenIlanlarLength = gelenIlanlar.length;
+      }
+      gelenIlanlar.reverse(); // gelen ilanları ters çeviriyoruz
+      this.ilanlar = []; // herdefasında veriyi sıfırlarız ki üzerine ekleme yapmasın
+      var i = 0;
+      gelenIlanlar.forEach(ilan => {
+        this.angularFire.database.object("users/" + ilan["ilaniVerenKullaniciId"]).subscribe(user => {
+          ilan["ilaniVerenKullanici"] = user;
+          ilan["random"] = "https://avatars.io/facebook/random" + i;
+          this.ilanlar.push(ilan);
+          i++;
+        });
+      });
+    })
+    this.ilanSayisi = this.ilanSayisi + 10;
+  }
+
+  ilanDetay(ilan) {
+    this.navCtrl.push(IlandetayPage, {
+      item: ilan
+    })
+  }
+
+  doRefresh(refresher) {
+    this.ilanSayisi = 10;
+    this.infiniteDahaFazla = true;
+    setTimeout(() => {
+      this.ionViewDidLoad();
+      refresher.complete();
+    }, 2000);
+  }
+
+  doInfinite(infiniteScroll) {
+    setTimeout(() => {
+      this.ionViewDidLoad();
+      infiniteScroll.complete();
+    }, 500);
   }
 
   ilanVer() {
     this.navCtrl.push(IlanverPage);
   }
 
+  searchComponenteGit() {
+    this.navCtrl.push(SearchComponent);
+  }
+
+
+  filtreAc() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('İlanları Filtrele');
+
+    alert.addInput({
+      type: 'text',
+      label: 'Baslangıç Yeri',
+      placeholder: 'Baslangıç Yeri: İstanbul, Kocaeli...'
+    });
+    alert.addInput({
+      type: 'text',
+      label: 'Bitiş Yeri',
+      placeholder: 'Bitiş Yeri: Mersin, Van...'
+    })
+    alert.addButton('Vazgeç');
+    alert.addButton({
+      text: 'Uygula',
+      handler: data => {
+        console.log(data);
+      }
+    });
+    alert.present();
+  }
+
+  /*     Anasayfa Slider Aktif olduğu zaman bu kısım aktif edilmeli
   onSlideChanged() {
     let currentIndex = this.slider.getActiveIndex();
     if (currentIndex == 0) {
@@ -41,36 +122,5 @@ export class AnasayfaPage {
       this.slider.slideTo(1, 100);
     }
   }
-
-  searchComponenteGit() {
-    this.navCtrl.push(SearchComponent);
-  }
-
-
-  filtreAc() {
-    let alert = this.alertCtrl.create();
-    alert.setTitle('İlanları Filtrele');
-
-    alert.addInput({
-      type: 'radio',
-      label: 'İlanlar',
-      value: 'ilan',
-      checked: true
-    });
-    alert.addInput({
-      type: 'radio',
-      label: 'Firmalar',
-      value: 'firma',
-      checked: false
-    })
-    alert.addButton('Vazgeç');
-    alert.addButton({
-      text: 'Uygula',
-      handler: data => {
-        console.log(data);
-      }
-    });
-    alert.present();
-  }
-
+  */
 }
